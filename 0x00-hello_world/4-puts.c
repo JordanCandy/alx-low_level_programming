0682077bb20fb2598523850825dfeab82fd15384
Jordan Candy <jordan.candy07@gmail.com>
2022-12-08 13:16:44 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - Prints string
+ * Return: 0
+ */
+
+int main(void)
+{
+	char firstString[] = "Programming is like building a multilingual puzzle";
+
+	puts(firstString);
+	return (0);
+}

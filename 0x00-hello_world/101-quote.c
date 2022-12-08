89fc7c0d5ee3c9d9c29b5a9a401601621578bf8b
Jordan Candy <jordan.candy07@gmail.com>
2022-12-08 15:40:24 -0800

    attempt at advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ *
+ * main - Entry point
+ * 
+ * Return: return 1 if successfuel
+ */
+
+int main(void)
+{
+	char MyChar[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+		putchar("%c\n", MyChar);
+	return (1);
+}

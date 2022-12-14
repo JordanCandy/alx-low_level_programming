e24be1476ad72ccaf81a509b5df97f628ce6524b
Jordan Candy <jordan.candy07@gmail.com>
2022-12-14 01:19:48 -0800

    rwo

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print_alphabet_x10 - Prints alphabet x10
+ * Result: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int j;
+	int x;
+
+	for (j = 0; j < 10; j++)
+	{
+		for ( x = 'a'; x = 'z'; x++)
+		{
+			putchar(x);
+		}
+		putchar('\n');
+	}
+}

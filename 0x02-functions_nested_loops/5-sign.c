535f9853add700044d8daeae3e659b058a1395a6
Jordan Candy <jordan.candy07@gmail.com>
2022-12-14 05:14:10 -0800

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - Prints size according to size
+ * @n: a parameter
+ * Return: return 1, 0 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}

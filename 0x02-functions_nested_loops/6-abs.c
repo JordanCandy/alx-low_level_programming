ba601eaf4d1eed9284c2b92572a07fd9a50b5988
Jordan Candy <jordan.candy07@gmail.com>
2022-12-14 05:30:08 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - prints th absolute value of int
+ * @x: parameter
+ * Return: Return x
+ */
+
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		x = -x;
+		return (x);
+	}
+}

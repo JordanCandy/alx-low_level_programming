b4000995944190fd8eb50d4331704f8bb59d7c9e
Jordan Candy <jordan.candy07@gmail.com>
2022-12-15 13:27:10 -0800

    pritnnumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 10; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+}

a2ea01c53b076d3e272296cd082c37382dbf732c
Jordan Candy <jordan.candy07@gmail.com>
2022-12-15 12:33:20 -0800

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - checks if int is a digit
+ * @c: parameter
+ * Return: return 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c < 10)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

877527cfc982b8a511b39930248b760e08b345bb
Jordan Candy <jordan.candy07@gmail.com>
2022-12-15 14:04:16 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - prints numbers 0 to 14 ten times
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int j, x;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			putchar(x);
+		}
+		putchar('\n');
+	}
+}

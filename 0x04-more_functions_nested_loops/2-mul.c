c22d1b74bf04e7b25affd4376ff6503406cba7ea
Jordan Candy <jordan.candy07@gmail.com>
2022-12-15 12:58:27 -0800

    Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Used to multiply two ints
+ * @a: parameter
+ * @b: parameter
+ * Return: return 0
+ */
+
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+
+	return (r);
+}

dea0408179fb478065589a557f6ee26c62788738
Jordan Candy <jordan.candy07@gmail.com>
2022-12-20 14:56:41 -0800

    string legnth

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - finds string length
+ * @s: parameter
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	*s = sizeof(s) - 1;
+
+	return (0);
+}

8d019b5627b441fd2268535aaa0722faf70b2562
Jordan Candy <jordan.candy07@gmail.com>
2022-12-21 11:35:15 -0800

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - prints sting in reverse
+ * @s: parameter
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int x;
+
+	for (x = 0; x > 0; x--)
+	{
+		putchar(s[x]);
+	}
+	putchar('\n');
+}
